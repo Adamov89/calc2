65c183de38921c120a6ce8370772902aad6f3f42
Adamov89 <40773158+Adamov89@users.noreply.github.com>
2019-07-08 01:00:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Calcalator
+{
+    class Calculator
+    {
+        public static double Dooperation(double a, double b, string h)
+        {
+            double c = double.NaN;
+
+            switch (h)
+            {
+                case "+":
+                    c = a + b;
+                    break;
+                case "-":
+                    c = a - b;
+                    break;
+                case "/":
+                    if (a != 0)
+                    {
+                        c = a / b;
+                    }
+                    break;
+                case "*":
+                    c = a * b;
+                    break;
+            }
+            return c;
+        }
+    }
+
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //bool endApp = false;
+
+            Console.WriteLine("Кулькулятор");
+            while (true)
+            {
+
+
+                double a, b, c; char h; c = 0.0;
+                Console.WriteLine("ввести первое число"); a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("ввести оператор (+) (-) (*) (/) "); h = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("ввести второе число"); b = Convert.ToDouble(Console.ReadLine());
+
+                //Console.WriteLine();
+                Console.WriteLine("= " + c);
+                Console.WriteLine("Начать заново? (y/n)");
+                ConsoleKeyInfo e = Console.ReadKey();
+                if (e.KeyChar == 'n') break;
+                Console.WriteLine();
+
+                //Console.WriteLine("Нажмите 'q' и Enter закрыть приложение, или нажмите любую другую клавишу и Enter, чтобы продолжить: ");
+                //if (Console.ReadLine() == "q") endApp = true;
+            }
+        }
+                
+    }
+}
